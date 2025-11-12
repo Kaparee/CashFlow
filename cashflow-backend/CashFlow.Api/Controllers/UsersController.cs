8da797210a40546ba142ed4184128e9774195ec3
CENTURIERS <168530093+CENTURIERS@users.noreply.github.com>
2025-11-12 16:14:59 +0100

    Ratunek 112

--- cashflow-backend/CashFlow.Api/Controllers/UsersController.cs
+++ cashflow-backend/CashFlow.Api/Controllers/UsersController.cs
@@ -18,7 +18,22 @@ public class UsersController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<User>>> GetUsers()
     {
-        return await _context.Users.ToListAsync();
+        // Używamy .Include() i .ThenInclude() do pobrania wszystkich powiązanych danych
+        var users = await _context.Users
+            // Wczytaj kolekcje bezpośrednio powiązane z User
+            .Include(u => u.Accounts)
+                // W ramach Accounts wczytaj walutę każdego konta
+                .ThenInclude(a => a.Currency)
+            .Include(u => u.Categories)
+                // W ramach Categories wczytaj słowa kluczowe (KeyWords)
+                .ThenInclude(c => c.KeyWords)
+            .Include(u => u.KeyWords) // Słowa kluczowe bezpośrednio przypisane do Użytkownika
+            .Include(u => u.Notifications)
+            .Include(u => u.RecTransactions) // Cykliczne transakcje
+                                             // Możesz dodać dalsze .Include() dla innych kolekcji, np. Limits
+            .ToListAsync();
+
+        return users;
     }
 }
 
